8df73f0498b5791018da367706fae75eb733817f
Dorin Esinenco <dorin.e.art@gmail.com>
2020-05-08 14:47:57 +0300

    Fixed some issues!

---
+++ src/examples/training/Main.java
@@ -0,0 +1,10 @@
+package examples.training;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("It's Working!");
+        System.out.println("Bye!!");
+        System.out.println("Good Good Bye!!");
+    }
+}
