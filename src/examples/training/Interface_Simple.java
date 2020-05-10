a9626121257e5042d897eba8bbb56330d82c27d1
plopa.natalia <plopa.natalia@acc.md>
2020-05-10 22:14:00 +0300

    interface simple

--- src/examples/training/Interface_Simple.java
+++ src/examples/training/Interface_Simple.java
@@ -1,4 +1,6 @@
 package examples.training;
 
 public interface Interface_Simple {
+    String getClassName();
+    int getClassCode();
 }
---
+++ src/examples/training/Material.java
@@ -0,0 +1,13 @@
+package examples.training;
+
+public class Material implements Interface_Simple{
+    @Override
+    public String getClassName() {
+        return "Material";
+    }
+    @Override
+    public int getClassCode() {
+        return 0;
+    }
+
+}
