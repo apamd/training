64fbc4ddb6c92cbf24799dfc84838c19c611b217
Livia165 <nicuvlad1997@mail.ru>
2020-05-11 13:31:48 +0300

    commitul initial

--- src/examples/training/Main.java
+++ src/examples/training/Main.java
@@ -3,7 +3,7 @@ package examples.training;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("It's Working!");
+        System.out.println("Apeduct/canalizare");
         System.out.println("Bye!!");
         System.out.println("Good Good Bye!!");
     }
