b356e614703a2dc2e5d31c19d0384af50bc5bfc6
vicky <mveronica@mail.ru>
2020-05-11 14:45:27 +0300

    change

---
+++ src/examples/training/Main.java
@@ -0,0 +1,10 @@
+package examples.training;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("It's work!!!");
+        System.out.println("Bye!!!");
+	// write your code here
+    }
+}
