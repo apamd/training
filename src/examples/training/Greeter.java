cfa639f773bf0c4d629402ba9b9aaa01c292774b
Dmitri Astafiev <demius.md@gmail.com>
2020-05-11 09:27:33 +0300

    test of create new branch

---
+++ src/examples/training/Greeter.java
@@ -0,0 +1,9 @@
+package examples.training;
+
+public class Greeter {
+    public static void greet() {
+        System.out.println("It's Working!");
+        System.out.println("Bye!!");
+        System.out.println("Good Good Bye!!");
+    }
+}
--- src/examples/training/Main.java
+++ src/examples/training/Main.java
@@ -3,8 +3,6 @@ package examples.training;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("It's Working!");
-        System.out.println("Bye!!");
-        System.out.println("Good Good Bye!!");
+        Greeter.greet();
     }
 }
